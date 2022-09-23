41b880d69cb8e7e73fb89fdb9a13d123a95bd4c6
Gabriel Gonzalez <gagoner@gmail.com>
2022-09-22 23:44:41 -0300

    Desafío 6

--- 6.- Websockets/index.js
+++ 6.- Websockets/index.js
@@ -1,37 +1,47 @@
 const express = require('express')
+const { Server } = require('socket.io')
+const handlebars = require('express-handlebars')
+const apiRouter = require('./routes/api.routes')
+const chatRouter = require('./routes/chat.router')
+const Manager = require('./controllers/chat.manager')
 const app = express()
-// const apiRouter = require('./routes/api.routes')
-// const { engine } = require('express-handlebars')
-// const path = require('path')
 const PORT = process.env.PORT || 8080
-
-// app.use(express.json())
-// app.use(express.urlencoded({ extended: true }))
-
-// app.engine(
-//     'handlebars',
-//     engine({
-//         extname: 'hbs',
-//         defaultLayout: 'main.hbs',
-//         layoutsDir: path.resolve(__dirname, './views/layouts'),
-//         partialsDir: path.resolve(__dirname, './views/partials')
-//     })
-// )
-// app.set('view engine', 'handlebars')
-// app.set('views', './views/')
-
-// app.use(express.static('public'))
-
-// app.use('/', apiRouter)
-
-app.get('/', (req, res) => {
-    res.send("Hello Desafío Coder!!!")
-})
-
-app.listen(PORT, (error) => {
+const server = app.listen(PORT, (error) => {
     if (error) {
         console.log(`Server error ${error}`)
     } else {
         console.log(`Server running on port ${PORT}`)
     }
+})
+const manager = new Manager()
+const io = new Server(server)
+let products = require('./models/product.model')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.engine('handlebars', handlebars.engine())
+
+app.set('view engine', 'handlebars')
+app.set('views', './views/')
+
+app.use('/content', express.static('./public'))
+
+app.use('/products', apiRouter)
+app.use('/chat', chatRouter)
+
+app.get('/', (req, res) => {
+    res.render('create-product')
+})
+
+io.on('connection', socket => {
+    console.log(`Client ${socket.id} connected...`)
+    socket.emit('history', products)
+    manager.findAll().then(result => socket.emit('chatHistory', result))
+    socket.on('products', data => {
+        io.emit('history', data)
+    })
+    socket.on('chat', data => {
+        io.emit('chatHistory', data)
+    })
 })
